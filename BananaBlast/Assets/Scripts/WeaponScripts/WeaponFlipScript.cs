87e6fc516276fc0df4d7ed7f5d97f8b61b0b6a91
RadiantCarrot <karlonng.rcy@gmail.com>
2023-07-20 10:12:59 +0800

    Final Submission Ready
    YAY

--- BananaBlast/Assets/Scripts/WeaponScripts/WeaponFlipScript.cs
+++ BananaBlast/Assets/Scripts/WeaponScripts/WeaponFlipScript.cs
@@ -9,27 +9,32 @@ public class WeaponFlipScript : MonoBehaviour
 {
     private Camera cam;
     private SpriteRenderer spriteRenderer;
+    public WeaponFreezerScript weaponFreezerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = FindObjectOfType<Camera>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        cam = FindObjectOfType<Camera>(); // assign camera
+        spriteRenderer = GetComponent<SpriteRenderer>(); // assign sprite renderer
+        weaponFreezerScript = GameObject.Find("WeaponHolder").GetComponent<WeaponFreezerScript>(); // assign freezer script
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position; // get direction of mouse cursor
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // point weapon in direction of cursor
+        if (weaponFreezerScript.weaponFreeze == false) // if weapon is not frozen
+        {
+            Vector3 lookDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position; // get direction of mouse cursor
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // point weapon in direction of cursor
 
-        if (angle < 89 && angle > -89)
-        {
-            spriteRenderer.flipY = false; // no flip gun
-        }
-        else
-        {
-            spriteRenderer.flipY = true; // flip gun
+            if (angle < 89 && angle > -89)
+            {
+                spriteRenderer.flipY = false; // no flip gun
+            }
+            else
+            {
+                spriteRenderer.flipY = true; // flip gun
+            }
         }
     }
 }
--- BananaBlast/Assets/Scripts/WeaponScripts/WeaponRotateScript.cs
+++ BananaBlast/Assets/Scripts/WeaponScripts/WeaponRotateScript.cs
@@ -8,20 +8,24 @@ using UnityEngine;
 public class WeaponRotateScript : MonoBehaviour
 {
     public Camera cam;
-    //public SpriteRenderer spriteRenderer;
+    public WeaponFreezerScript weaponFreezerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = FindObjectOfType<Camera>();
+        cam = FindObjectOfType<Camera>(); // assign camera
+        weaponFreezerScript = GameObject.Find("WeaponHolder").GetComponent<WeaponFreezerScript>(); // assign freezer script
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position; // get direction of mouse cursor
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // point weapon in direction of cursor
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f); // reset weaponholder rotation
-        transform.rotation = Quaternion.Euler(0f, 0f, angle); // assigns direction to weapon
+        if (weaponFreezerScript.weaponFreeze == false) // if weapon is not frozen
+        {
+            Vector3 lookDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position; // get direction of mouse cursor
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // point weapon in direction of cursor
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f); // reset weaponholder rotation
+            transform.rotation = Quaternion.Euler(0f, 0f, angle); // assigns direction to weapon
+        }
     }
 }
